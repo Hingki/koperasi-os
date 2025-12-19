f8ac480491e9003850f6f056b5e1059360666669
FHC <hingkicandra@gmail.com>
2025-12-20 01:53:00 +0700

    chore: fix eslint to ignore build files

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+﻿import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import globals from "globals";
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ignores: [
+      "**/.next/**",
+      "**/node_modules/**",
+      "**/scripts/**",
+      "**/*.config.js",
+      "**/dist/**",
+      "**/build/**",
+      "**/.git/**",
+      "**/.vercel/**",
+      "**/coverage/**",
+      "**/*.min.js"
+    ],
+  },
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+      }
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }]
+    }
+  }
+);
